6d85a3ba185a74bb34fcb40422dcfe0ec717e955
Anna <stepanova.ao@gmail.com>
2022-08-13 12:57:52 +0300

    create project

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(1);
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+   count: numberOfFilms,
+   movies: {},
+   actors: {},
+   genres: [],
+   privat: false,
+};
+let a = prompt("Один из последний просмотренных фильмов?", ""),      
+    b = prompt("На сколько оцените его?", ""),
+    c = prompt("Один из последний просмотренных фильмов?", ""),  
+    d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] = d; 
+
+console.log(personalMovieDB);
