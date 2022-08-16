187d695d05e6a949839637e41dc6c5a80fce3b4e
Anna <stepanova.ao@gmail.com>
2022-08-16 09:30:23 +0300

    add functions

--- script.js
+++ script.js
@@ -1,33 +1,81 @@
 "use strict";
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
 
-if (numberOfFilms < 10) {
-    alert('Мало фильмов');
-} else if (numberOfFilms<30) {
-    alert('Вы классический зритель');
-} else if (numberOfFilms>30) {
-    alert("Вы киноман");
-} else {
-    alert("Произошла ошибка")
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+    }
 }
+
+start();
+
 const personalMovieDB = {
    count: numberOfFilms,
    movies: {},
    actors: {},
    genres: [],
    privat: false,
-};
+}; 
 
-for (let i = 0; i<2 ; i++) {
+function rememberMyFilms() {
+    for (let i = 0; i<2 ; i++) {
+        let a = prompt("Один из последний просмотренных фильмов?", ""),
+            b = prompt("На сколько оцените его?", "");
+        if (a != "" && b != "" && a != null && b != null && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+        } else {
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (numberOfFilms < 10) {
+        alert('Мало фильмов');
+    } else if (numberOfFilms<30) {
+        alert('Вы классический зритель');
+    } else if (numberOfFilms>30) {
+        alert("Вы киноман");
+    } else {
+        alert("Произошла ошибка")
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB() {
+    if (!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+function writeYourGenres() {
+    for (let i=0; i<3; i++) {
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}?`);
+    }
+}
+
+writeYourGenres();
+
+//console.log(personalMovieDB);
+
+
+/* for (let i = 0; i<2 ; i++) {
     let a, b;
     do {a = prompt("Один из последний просмотренных фильмов?", ""),
         b = prompt("На сколько оцените его?", "")}
     while (a == "" || a == null || b == "" || b == null || a.length > 50 );
     personalMovieDB[a] = b;
-}
+} 
 
-/* for (let i = 0; i<2 ; i++) {
+for (let i = 0; i<2 ; i++) {
     let a = prompt("Один из последний просмотренных фильмов?", "");
     while (a == "" || a == null || a.length > 50) {
        a = prompt("Один из последний просмотренных фильмов?", ""); 
@@ -39,22 +87,4 @@ for (let i = 0; i<2 ; i++) {
     personalMovieDB.movies[a] = b;
 } */
 
-/* for (let i = 0; i<2 ; i++) {
-    let a = prompt("Один из последний просмотренных фильмов?", ""),
-        b = prompt("На сколько оцените его?", "");
-    if (a != "" && b != "" && a != null && b != null && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-    } else {
-        i--;
-    }
-} */
 
-/* let a = prompt("Один из последний просмотренных фильмов?", ""),      
-    b = prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последний просмотренных фильмов?", ""),  
-    d = prompt("На сколько оцените его?", "");
- */
-/* personalMovieDB.movies[a] = b; 
-personalMovieDB.movies[c] = d;  */
-
-console.log(personalMovieDB);
